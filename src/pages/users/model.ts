674b39e065c2b37d99ef83190f337a446785c7c0
Chandler <testchn@163>
2021-11-30 23:37:38 +0800

    first commit

---
+++ src/pages/users/model.ts
@@ -0,0 +1,22 @@
+
+const UserModel = {
+    namespace: "users",
+    state: {},
+    reducers: {
+        getList(){
+            // return DataCue;
+        }
+    },
+    effect: {
+        fun(){
+            //yield put()
+        }
+    },
+    substcriptions: {
+        function_name(param) {
+            //body
+            
+        }
+    }
+};
+export default UserModel;
