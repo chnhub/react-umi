b93bdde2a2b90d46fc570dc4a8b761f89d55cf37
Chandler <testchn@163>
2022-03-21 21:06:58 +0800

    优化design的select

--- src/pages/ModelDesign/index.tsx
+++ src/pages/ModelDesign/index.tsx
@@ -1,39 +1,63 @@
 
 import { request } from '@/.umi/plugin-request/request'
 import { FooterToolbar } from '@ant-design/pro-layout'
-import { SchemaForm, SchemaMarkupField as Field } from '@formily/antd'
-import { Input, ArrayTable, Select, Checkbox, FormCard } from '@formily/antd-components'
+import { SchemaForm, SchemaMarkupField as Field, FormEffectHooks, createFormActions, createAsyncFormActions, FormButtonGroup } from '@formily/antd'
+import { Input, ArrayTable, Select, Checkbox, FormCard, Submit } from '@formily/antd-components'
 import 'antd/dist/antd.css'
 import { useEffect } from 'react'
 import { Button } from '_antd@4.19.2@antd'
 import { useRequest, history, useLocation } from 'umi';
 
+import * as enums from './enums'
+
 const Index = () => {
-    const init = useRequest('antd/api/models/design/48', {
+    //监控
+    const { onFieldValueChange$, onFieldChange$ } = FormEffectHooks;
+    //更改元素状态
+    // const { setFieldState } = createFormActions();
+    // const { setFieldState } = createAsyncFormActions();
+    const init = useRequest('antd/api/models/design/37', {
         manual: true,
     });
-    const { data, ...msg } = init.data;
+
     useEffect(() => {
         if (init) {
             init.run();
             console.log('init', init);
         }
     }, [])
+    const data = init?.data?.data;
+
     return (
-        <SchemaForm actions={null} components={{ ArrayTable, Input, Select, Button, Checkbox }}>
+        <SchemaForm initialValues={data} actions={null} components={{ ArrayTable, Input, Select, Button, Checkbox }}
+            effects={(_, { setFieldState }) => {
+                onFieldValueChange$('fieldsCard.fields.*.type').subscribe((i) => {
+                    console.log(i);
+                    if (i.value === 'switch' || i.value === 'radio') {
+                        setFieldState(i.path.replace('type', 'data'), state => {
+                            state.editable = true;
+                        })
+                    } else {
+                        setFieldState(i.path.replace('type', 'data'), state => {
+                            state.editable = false;
+                        })
+                    }
+                })
+                onFieldChange$('fieldsCard.fields.*.data').subscribe((i) => {
+                    console.log(i);
+                });
+
+            }}
+            onSubmit={v => console.log(v)}
+        >
             <FormCard title="Basic">
-                <Field name="route" x-component="Input" title="Route Name:" />
+                <Field name="routeName" x-component="Input" title="Route Name:" />
             </FormCard>
-            <FormCard title="Fields">
-                <Field type="object"
+            <FormCard title="Fields" name='fieldsCard'>
+                <Field type="array"
                     // title=""
-                    name="userList"
-                    type="array"
+                    name="fields"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', name: 20 },
-                        { title: 'joe', name: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -44,31 +68,27 @@ const Index = () => {
                     <Field type="object">
                         <Field name="title" x-component="Input" title="Title" />
                         <Field name="name" x-component="Input" title="Name" />
-                        <Field name="type" x-component="Select" title="Type" />
+                        <Field name="type" x-component="Select" title="Type" enum={enums.fieldType} />
                         <Field
                             title="Data"
                             name="data"
+                            editable={false}
                             x-component="Button"
                             x-component-props={{
                                 children: 'Data',
                             }}
                         />
                         <Field name="listSorter" x-component="Checkbox" title="List Sorter" />
-                        <Field name="hide" x-component="Checkbox" title="Hide InColumn" />
-                        <Field name="edit" x-component="Checkbox" title="Edit Disabled" />
+                        <Field name="hideInColumn" x-component="Checkbox" title="Hide InColumn" />
+                        <Field name="editDisabled" x-component="Checkbox" title="Edit Disabled" />
                     </Field>
                 </Field>
             </FormCard>
-            <FormCard title="List Action">
-                <Field type="object"
+            <FormCard title="List Action" >
+                <Field type="array"
                     // title=""
                     name="listAction"
-                    type="array"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', uri: 20 },
-                        { title: 'joe', uri: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -87,15 +107,10 @@ const Index = () => {
                 </Field>
             </FormCard>
             <FormCard title="Edit Action">
-                <Field type="object"
+                <Field type="array"
                     // title=""
                     name="editAction"
-                    type="array"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', uri: 20 },
-                        { title: 'joe', uri: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -113,15 +128,10 @@ const Index = () => {
                 </Field>
             </FormCard>
             <FormCard title="Table Toolbar">
-                <Field type="object"
+                <Field type="array"
                     // title=""
                     name="editAction"
-                    type="array"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', uri: 20 },
-                        { title: 'joe', uri: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -139,15 +149,10 @@ const Index = () => {
                 </Field>
             </FormCard>
             <FormCard title="Batch Toolbar">
-                <Field type="object"
+                <Field type="array"
                     // title=""
                     name="batchToolbar"
-                    type="array"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', uri: 20 },
-                        { title: 'joe', uri: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -165,15 +170,10 @@ const Index = () => {
                 </Field>
             </FormCard>
             <FormCard title="Batch Toolbar - Trashed">
-                <Field type="batchToolbarTrashed"
+                <Field type="array"
                     // title=""
-                    name="batchToolbar"
-                    type="array"
+                    name="batchToolbarTrashed"
                     x-component="ArrayTable"
-                    default={[
-                        { title: 'morally', uri: 20 },
-                        { title: 'joe', uri: 21 }
-                    ]}
                     x-component-props={{
                         operationsWidth: '30%',
                         operations: {
@@ -190,7 +190,7 @@ const Index = () => {
                     </Field>
                 </Field>
             </FormCard>
-            <FooterToolbar extra={<Button type='primary'>Submit</Button>}></FooterToolbar>
+            <FooterToolbar extra={<FormButtonGroup><Submit /></FormButtonGroup>}></FooterToolbar>
         </SchemaForm >
 
     )
